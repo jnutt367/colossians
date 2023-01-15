4de14a7ef62eb5007d6199465791eb1ebaffe321
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-14 22:49:38 -0500

    Update index.js
    Colossians_1.jpg

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     </h1>
     <div className={styles.hero}>
        <Image 
-                                src="/Colossians_1_833_460_80_c1.jpg"
+                                src="/Colossians_1.jpg"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}
